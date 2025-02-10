11deb4aa301959e1fba18d5c5f829e0cc63b2434
LuanAmaral <luan.rocha.amaral@gmail.com>
2025-02-10 20:39:52 -0300

    First

---
+++ main/smartwatch.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "freeRTOS/FreeRTOS.h"
+#include "freeRTOS/task.h"
+#include "esp_log.h"
+
+
+#ifdef __cplusplus
+extern "C" 
+{
+    void app_main(void);
+}
+#endif
+
+
+void app_main(void)
+{
+
+}
